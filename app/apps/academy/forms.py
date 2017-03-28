f6f44188d15bc29dc471366b4ca076eb6d0360e1
wrufesh singh <wrufesh@gmail.com>
2017-03-28 13:39:56 +0545

    checkbox applied to toggle binding

--- app/apps/academy/forms.py
+++ app/apps/academy/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from app.apps.academy.models import BoardOrUniversity, Faculty, Program, ProgramLevel, Course, Institute, ChapterPage, \
     Option, Question
 from app.utils.forms import HTML5BootstrapModelForm
+from django.utils.translation import ugettext as _
 
 
 class InstituteForm(HTML5BootstrapModelForm):
@@ -55,7 +56,6 @@ class CourseForm(HTML5BootstrapModelForm):
 
 
 class ChapterForm(HTML5BootstrapModelForm):
-
     class Meta:
         model = ChapterPage
         fields = '__all__'
@@ -66,9 +66,15 @@ class OptionForm(HTML5BootstrapModelForm):
         model = Option
         fields = ('detail', 'is_correct')
 
+        labels = {
+            'detail': _('Choice'),
+            'is_correct': _('Is correct choice')
+        }
+
         widgets = {
             'detail': forms.TextInput(attrs={'data-bind': 'value: detail'}),
-            'is_correct': forms.CheckboxInput(attrs={'data-bind': 'checked: detail'})
+            'is_correct': forms.CheckboxInput(
+                attrs={'data-bind': 'checked: is_correct, bstoggle: {"on": "Yes", "off": "No"}'})
         }
 
 
@@ -77,10 +83,18 @@ class QuestionForm(HTML5BootstrapModelForm):
         # 'detail', 'image', 'true_false_answer', 'type', 'choices'
         model = Question
         fields = ('detail', 'image', 'true_false_answer', 'type')
+
+        labels = {
+            'detail': _('Question'),
+            'image': _('Upload Question Image'),
+            'true_false_answer': _('Answer Is'),
+            'type': _('Question Type')
+        }
+
         widgets = {
             'detail': forms.TextInput(attrs={'data-bind': 'value: detail'}),
             'image': forms.ClearableFileInput(attrs={'data-bind': 'value: image'}),
             'type': forms.Select(attrs={'data-bind': 'value: type'}),
-            'true_false_answer': forms.CheckboxInput(attrs={'data-bind': 'checked: true_false_answer, visible: type() == "TRUE/FALSE"'})
+            'true_false_answer': forms.CheckboxInput(
+                attrs={'data-bind': 'checked: true_false_answer, bstoggle: {"on": "Yes", "off": "No"}'})
         }
-
